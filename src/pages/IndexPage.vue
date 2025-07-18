b112ae78aca59354175f02cc1fdd3aa0c543a73e
narges.karimian <nargeskarimian1551@gmail.com>
2025-07-18 11:24:04 +0330

    feat: map using vue-leaflet

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,24 +1,92 @@
 <template>
-  <q-page class="flex flex-center">
-    <div id="map" style="height: 400px; width: 400px"></div>
-    <!-- <mapir :center="center" :apiKey="apiKey"></mapir> -->
+  <q-page class="flex flex-center main-view">
+    <div class="main-view__map-container" dir="ltr">
+      <l-map ref="map" v-model:zoom="zoom" :center="center" :options="{ zoomControl: false }">
+        <l-tile-layer
+          :url="getEnv('OPEN_STREET_TILE_API')"
+          layer-type="base"
+          name="OpenStreetMap"
+        />
+        <l-marker v-if="selectedLocation.marker" :lat-lng="selectedLocation.marker"></l-marker>
+      </l-map>
+    </div>
+
+    <SearchBox v-if="!selectedLocation.showModal" @select-location="selectLocation" />
+    <BuildingInfoDialog
+      v-model="selectedLocation.showModal"
+      :location="selectedLocation.data"
+      @update:model-value="closeSelectedLocation"
+      @edit-building-info="editBuildingInfo"
+      @edit-pip="editPip"
+    />
   </q-page>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
-import OSMMap from 'map'
-// import { mapir } from 'mapir-vue'
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import SearchBox from 'src/components/SearchBox.vue'
+import BuildingInfoDialog from 'src/components/‌BuildingInfoDialog.vue'
+import { getEnv } from 'src/utils'
 
+const zoom = ref(12)
 const map = ref(null)
+const center = ref([36.2974945, 59.6059232])
+const router = useRouter()
 
-onMounted(() => {
-  map.value = new OSMMap('map', {
-    center: [51.420296, 35.732379], // New York
-    zoom: 12,
-  })
+const selectedLocation = reactive({
+  marker: null,
+  data: null,
+  showModal: false,
 })
-// const apiKey =
-//   'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImIwODgyOWJjMDc3YzM0Mzc1NzE4OWQ0N2E2YzY1MjhmMjFjNmY0NmI3OGQxMGUwZDAzODFjYjMyYzM4NzI0M2IzMzFlZmJiMzQ3MjdhNTU3In0.eyJhdWQiOiIzMTc0MCIsImp0aSI6ImIwODgyOWJjMDc3YzM0Mzc1NzE4OWQ0N2E2YzY1MjhmMjFjNmY0NmI3OGQxMGUwZDAzODFjYjMyYzM4NzI0M2IzMzFlZmJiMzQ3MjdhNTU3IiwiaWF0IjoxNzQzNDk3NjU4LCJuYmYiOjE3NDM0OTc2NTgsImV4cCI6MTc0NjA4OTY1OCwic3ViIjoiIiwic2NvcGVzIjpbImJhc2ljIl19.QO7S7ztqKvdVCnXnkvUyxHxzRHyBD5HNJXu1-ccPiGnznm-E_PfQX6Os04uq34YdjgVR2OOCMyBP4Lj0RpRKcTJfBOvw1yHp7hp7gBwRIk2EPxWbv3pbBB0DqgilK2PmJVPVX8qt58CLZ1nuh-f4cl8i0vYwlzHUTIvFH6NU9Q8FgdzPOZ8uWzfmCJxO_iQHuIigDpkpOLrWCO77PCokAIj1ZU-6E3o-5IXgYNC5W2UuHKH26JL03Xd_l5JSiG9VuqNLID3OHMLznzOm8m6X4hfam1WetAJFYhKXiily10PTS_5N7iKQEG2QrRpuH8SMvPZBCiM1Tlw1q9Yit_KgKw'
-// const center = ref([51.420296, 35.732379])
+
+// Fix for default markers not working with bundlers
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+})
+
+const selectLocation = (location) => {
+  selectedLocation.data = location
+  selectedLocation.showModal = true
+
+  const lat = parseFloat(location.lat)
+  const lon = parseFloat(location.lon)
+  selectedLocation.marker = [lat, lon]
+
+  center.value = [lat, lon]
+  map.value?.leafletObject.flyTo([lat, lon], 17)
+  setTimeout(() => {
+    zoom.value = 17
+  }, 500)
+}
+
+const closeSelectedLocation = () => {
+  selectedLocation.data = null
+  selectedLocation.marker = null
+  selectedLocation.showModal = false
+}
+
+const editBuildingInfo = () => {
+  console.log('editBuildingInfo')
+  router.push({ name: 'edit-building', params: { id: selectedLocation.data.id } })
+}
+
+const editPip = () => {
+  console.log('editPip')
+}
 </script>
+
+<style lang="scss" scoped>
+.main-view {
+  &__map-container {
+    width: 100%;
+    height: 100vh;
+  }
+}
+</style>
