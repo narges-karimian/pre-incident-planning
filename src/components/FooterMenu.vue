a30a79e6b0e26203cf8527bd2fda3418ff5d7fa5
narges.karimian <nargeskarimian1551@gmail.com>
2025-07-18 11:28:15 +0330

    feat: change main layout

---
+++ src/components/FooterMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-tabs
+    no-caps
+    active-color="primary"
+    indicator-color="transparent"
+    class="text-grey-8 footer-menu"
+    v-model="tab"
+  >
+    <q-tab name="map" label="نقشه" icon="location_on" />
+    <q-tab name="setting" label="تنظیمات من" icon="account_circle" />
+  </q-tabs>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const tab = ref('map')
+</script>
+
+<style scoped>
+.footer-menu {
+  :deep(.q-tab__label) {
+    font-size: 12px;
+    font-weight: 200;
+  }
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,15 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title> برنامه ریزی پیش از حادثه </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
+  <q-layout view="lHh Lpr lFf" class="my-font">
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer bordered class="bg-grey-3">
+      <FooterMenu />
+    </q-footer>
   </q-layout>
 </template>
 
-<script setup></script>
+<script setup>
+import FooterMenu from 'src/components/FooterMenu.vue'
+</script>
