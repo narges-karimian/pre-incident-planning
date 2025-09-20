3b69e497ac3e95abb3872a56dce6dbd71a229eb9
narges.karimian <nargeskarimian1551@gmail.com>
2025-09-20 11:18:07 +0330

    feat: icon details

---
+++ src/utils/hazardIconData/index.js
@@ -0,0 +1,29 @@
+import { AccessHazards } from './AccessHazards.json'
+import { HazardousMaterials } from './HazardousMaterials.json'
+import { HazardPointsDetailed } from './HazardPointsDetailed.json'
+import { HumanCausedHazards } from './HumanCausedHazards.json'
+import { Incident } from './Incident.json'
+import { Infrastructure } from './Infrastructure.json'
+import { Lifelines } from './Lifelines.json'
+import { NaturalHazards } from './NaturalHazards.json'
+import { NIMSPositions } from './NIMSPositions.json'
+import { Preplan } from './Preplan.json'
+import { PublicAlert } from './PublicAlert.json'
+import { Resources } from './Resources.json'
+import { USAR } from './USAR.json'
+
+export {
+  AccessHazards,
+  HazardousMaterials,
+  HazardPointsDetailed,
+  HumanCausedHazards,
+  Incident,
+  Infrastructure,
+  Lifelines,
+  NaturalHazards,
+  NIMSPositions,
+  Preplan,
+  PublicAlert,
+  Resources,
+  USAR,
+}
